75246fb029a4c62e770cfd1e331d507392df55d3
Aip Villanueva <aipgvillanueva@gmail.com>
2026-04-15 21:02:48 +0800

    Update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,6 @@ import Ui.LoginScreen;
 
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new LoginScreen());
+        new LoginScreen();
     }
 }
